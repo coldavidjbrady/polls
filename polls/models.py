de9c6177ddcf18f31b9496febbaf9ac7bc2540d8
coldavidjbrady <coldavidjbrady@gmail.com>
2017-05-22 17:45:03 -0700

    committed all needed files in polls

---
+++ polls/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+# the following lines added:
+import datetime
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    # In the Django Admin section, each model has a property called list_display which controls which fields are
+    # displayed on the change list page of the admin page. In this case "was_published_recently" is a custom column
+    # and the sort is ordered by the pub_date attribute (Refer to part 7 of the Django Poll tutorial for more detail).
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length = 200)
+    votes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.choice_text
---
+++ polls/views.py
@@ -0,0 +1,67 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from .models import Question, Choice
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # request.Post is a dictionary-like object that lets you access submitted data by key name.
+        # In this case, request.POST['choice'] returns the ID of the selected choice, as a string.
+        # request.POST values are always strings.
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    selected_choice.votes += 1
+    selected_choice.save()
+    # Always return an HttpResponseRedirect after successfully dealing with POST data. This prevents data from
+    # being posted twice if a user hits the Back button.
+    # The reverse function has access to the URL map that Django uses to find a view function for incoming URLs.
+    # In this case, you pass in a view function, and the arguments it will get, and it finds the URL that maps to it.
+    # Then the HttpResponseRedirect function creates a response that directs the browser to visit that URL.
+    return HttpResponseRedirect(reverse('poll:results', args=(question.id,)))
+
+
+'''
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # The context is a dictionary mapping template variable names to Python objects.
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'Poll/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'Poll/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'Poll/results.html', {'question': question})
+'''
