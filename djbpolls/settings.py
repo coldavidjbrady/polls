1b38ea381e8cd5ffe3b2a0d6e787731f6b95a492
davidjbrady <coldavidjbrady@gmail.com>
2022-11-20 10:49:10 -0800

    updated settings

--- djbpolls/settings.py
+++ djbpolls/settings.py
@@ -14,7 +14,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import dj_database_url
-from .local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,6 +123,17 @@ MEDIA_ROOT = BASE_DIR + '/media/'
 
 
 # Update database configuration with $DATABASE_URL.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Go Army!',
+        'USER': 'Beat Navy!!',
+        'PASSWORD': 'AGAIN AND AGAIN!!!',
+        'HOST': 'West Point',
+        'PORT': '5432',
+
+    }
+}
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
